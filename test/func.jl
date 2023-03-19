ed94330c545e090ae450f086dea6db83bcfe0e94
kiwikuma <anfangwf@outlook.com>
2023-03-19 21:07:25 +0100

    test

--- test/func.jl
+++ test/func.jl
@@ -55,7 +55,7 @@ args = (K, F, J, b, psubgrid)
 argsOne = (F,K)
 probOne = ODEProblem(Lorenz96One_shift!, u0One, (T0,Tmax), argsOne);
 solOne = solve(probOne, dt=dt,saveat=dt);
-
+solOneX = solOne[:,:]
 
 probTwo = ODEProblem(Lorenz96Two_shift_LR!, u0Two_LR, (T0,Tmax), args);
 solTwo = solve(probTwo, dt=dt, saveat=dt);
@@ -63,7 +63,8 @@ solTwo = solve(probTwo, dt=dt, saveat=dt);
 solTwo_t = solTwo.t
 solTwoX = solTwo[1:K,:]
 solTwoY = solTwo[K+1:end,:]
-
-@test solOne[:,:] ≈ exact_solOneX
+println(size(solOne))
+println(size(exact_solOneX))
+@test solOneX ≈ exact_solOneX
 @test solTwoX≈exact_solTwoX_LR
 @test solTwoY≈exact_solTwoY_LR
